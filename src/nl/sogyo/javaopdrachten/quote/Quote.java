c865c8429dff8484a0a0b01b0f36c51f433605e6
Tos Alles <talles@sogyo.nl>
2015-10-06 16:50:22 +0200

    Tekstbestanden met data voor opgaves toegevoegd, String[][] voor Quotes opgave opgegeven

---
+++ src/nl/sogyo/javaopdrachten/quote/Quote.java
@@ -0,0 +1,16 @@
+package nl.sogyo.javaopdrachten.quote;
+
+public class Quote {
+    String[][] quotes = {
+        {"galileo", "eppur si muove"},
+        {"achimedes", "eureka!"},
+        {"erasmus", "in regione caecorum rex est luscus"},
+        {"socrates", "I know nothing except the fact of my ignorance"},
+        {"rené descartes", "cogito, ergo sum"},
+        {"sir isaac newton", "if I have seen further it is by standing on the shoulders of giants"}
+    };
+
+    public static void main(String... args) {
+        //TODO
+    }
+}
